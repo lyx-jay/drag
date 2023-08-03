0935b4e606b490e8c305ff854152b6c4a2d59668
lvyuanxin <1498781418@qq.com>
2023-08-03 22:33:41 +0800

    feat: add mouseenter listener

--- src/drag.ts
+++ src/drag.ts
@@ -1,11 +1,20 @@
 
+// 鼠标按下
 const mouseDown = (e: Event) => {
   console.log(e.target)
 }
 
+// 鼠标松开
 const mouseUp = (e: Event) => {
   console.log(e.target)
 }
+
+// 鼠标进入元素会触发该事件
+const mouseEnter = (e: Event) => {
+
+}
+
+
 
 const parentEl = document.getElementById('wrapper')
 const childNodes = parentEl?.childNodes
@@ -14,7 +23,8 @@ console.log('childNodes', childNodes)
 childNodes && childNodes.forEach(node => {
   if (node.nodeType === 1) {
     node.addEventListener('mousedown', mouseDown)
-    node.addEventListener('mouseup', mouseUp)
+    node.addEventListener('mouseEnter', mouseUp)
   }
 })
 
+window.addEventListener('mouseup', mouseUp)
